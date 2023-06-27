ff8ecbbdcba049be42c04531ec35158e03ff9920
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-06-27 08:31:38 +0530

    Add files via upload

---
+++ NewsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.JavaFull.NewsPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPortalApplication.class, args);
+	}
+
+}
